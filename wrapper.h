28c9dc4f0fecc5700cf76197959f60f74514d072
Iuri Diniz <iuridiniz@gmail.com>
2021-07-05 21:57:02 -0300

    remove garbage

--- wrapper.h
+++ wrapper.h
@@ -4,36 +4,3 @@
 
 //clang `pkg-config --cflags sofia-sip-ua` wrapper.h
 //cpp `pkg-config --cflags sofia-sip-ua` wrapper.h
-// int tttttttttttttttt;
-// TAG_NEXT(a);
-// NTATAG_USER_VIA(1),
-//     TPTAG_PONG2PING(1),
-//     NTATAG_TCP_RPORT(0),
-//     NTATAG_TLS_RPORT(0),
-//     NUTAG_RETRY_AFTER_ENABLE(0),
-//     NUTAG_AUTO_INVITE_100(0),
-//     TAG_IF(!strchr(profile->sipip, ':'), SOATAG_AF(SOA_AF_IP4_ONLY)),
-//     TAG_END()
-
-// TAG_NEXT(A);
-// tag_next, (tag_value_t)(A);
-// NUTAG_URL("*:5060");
-// nutag_url, urltag_url_v("*:5060");
-
-// NUTAG_URL(url),
-//     SIPTAG_SUBJECT_STR("NUA-11.1.1"),
-//     SIPTAG_CONTENT_TYPE_STR("text/plain"),
-//     SIPTAG_PAYLOAD_STR("Hello hellO!\n"),
-//     TAG_END()
-
-// nutag_url, urltag_url_v(url),
-//     siptag_subject_str, tag_str_v("NUA-11.1.1"),
-//     siptag_content_type_str, tag_str_v("text/plain"),
-//     siptag_payload_str, tag_str_v("Hello hellO!\n"),
-//     (tag_type_t)0, (tag_value_t)0
-// SU_WAIT_FOREVER;
-
-//URL_PRINT_FORMAT;
-//URL_PRINT_ARGS(u);
-//"%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s";
-// (u)->url_scheme ? (u)->url_scheme : "", (u)->url_type != url_any && (u)->url_scheme && (u)->url_scheme[0] ? ":" : "", (u)->url_root && ((u)->url_host || (u)->url_user) ? "//" : "", (u)->url_user ? (u)->url_user : "", (u)->url_user && (u)->url_password ? ":" : "", (u)->url_user && (u)->url_password ? (u)->url_password : "", (u)->url_user && (u)->url_host ? "@" : "", (u)->url_host ? (u)->url_host : "", (u)->url_host && (u)->url_port ? ":" : "", (u)->url_host && (u)->url_port ? (u)->url_port : "", (u)->url_root && (u)->url_path ? "/" : "", (u)->url_path ? (u)->url_path : "", (u)->url_params ? ";" : "", (u)->url_params ? (u)->url_params : "", (u)->url_headers ? "?" : "", (u)->url_headers ? (u)->url_headers : "", (u)->url_fragment ? "#" : "", (u)->url_fragment ? (u)->url_fragment : "";
