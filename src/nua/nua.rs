a38c146a1401195827c245b12df54b7930c697bb
Iuri Diniz <iuridiniz@gmail.com>
2021-07-15 00:26:27 -0300

    comments

--- src/nua/nua.rs
+++ src/nua/nua.rs
@@ -257,7 +257,7 @@ extern "C" fn nua_callback_glue(
 ) {
     // println!("------ nua_callback_glue ------");
 
-    //dbg!(&_event, &_status, &_phrase, &_nua, &_magic, &_nh, &_hmagic, &_sip, &_tags);
+    // dbg!(&_event, &_status, &_phrase, &_nua, &_magic, &_nh, &_hmagic, &_sip, &_tags);
 
     /*
     Panics can happen pretty much anywhere in Rust code.
@@ -268,7 +268,7 @@ extern "C" fn nua_callback_glue(
         /* This call is expect to not panic if sofia does not changes their api */
         /* Also, it can happen if memory is corrupted and the process must be aborted, anyway */
         let event: Event = Event::try_from(_event as i32).unwrap();
-        //dbg!(&event);
+        // dbg!(&event);
         let status = _status as u32;
 
         let phrase: String = unsafe { CStr::from_ptr(_phrase).to_string_lossy().into_owned() };
@@ -277,7 +277,7 @@ extern "C" fn nua_callback_glue(
 
         let nua: *mut Nua = _magic as *mut Nua;
 
-        /* sanity check */
+        /* sanity check for Nua */
         {
             assert!(!nua.is_null());
             let nua: &Nua = unsafe { &*nua };
@@ -288,6 +288,7 @@ extern "C" fn nua_callback_glue(
 
         let handle: *mut Handle = _hmagic as *mut Handle;
         // let handle_struct: Option<&Handle>;
+        /* sanity check for Handle */
         if !handle.is_null() {
             /* reply to an owned handle function (outgoing sip message) */
             let handle: &Handle = unsafe { &*handle };
@@ -319,7 +320,8 @@ extern "C" fn nua_callback_glue(
         // Code here must be panic-free.
         let error = format!("PANIC!! while calling a callback from C: {:?}\n\0", e);
         eprint!("{}", &error);
-        unsafe { sys::printf(error.as_ptr() as *const i8) };
+        // println!("{}", e);
+        // unsafe { sys::printf(error.as_ptr() as *const i8) };
         // Abort is safe because it doesn't unwind.
         std::process::abort();
     }
